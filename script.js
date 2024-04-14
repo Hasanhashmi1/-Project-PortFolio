5a6b35422ff2ff744b491222b37956e2157e4dbc
Hasan hashmi <hashmihasan189@gmail.com>
2024-04-14 20:01:52 +0530

    Added CSS JS

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ var currentIndex = 0;
 function changeText() {
   var changingText = document.getElementById("changing-text");
   var currentText = texts[currentIndex];
-  
+
   // Reset text content
   changingText.textContent = "";
-  
+
   // Typing effect animation
   var i = 0;
-  var typingInterval = setInterval(function() {
+  var typingInterval = setInterval(function () {
     // Append one letter to the text content
     changingText.textContent += currentText[i];
     i++;
@@ -27,7 +27,7 @@ function changeText() {
       clearInterval(typingInterval);
     }
   }, 100); // Adjust typing speed as needed
-  
+
   // Increment index and reset if exceeding array length
   currentIndex++;
   if (currentIndex >= texts.length) {
@@ -46,13 +46,57 @@ setInterval(changeText, 5000);
 
 
 
+// Here I'm going to create a scrollto funtion which will scroll to a perticular div
+document.addEventListener("DOMContentLoaded", () => {
+  var aboutLink = document.getElementById("aboutLink");
+  var mySection = document.getElementById("fade-in-section2");
+  var myProjectsBtn = document.getElementById("projectList");
+  var myProjects = document.getElementById("myProjects");
+  var colY = document.getElementById("nav1");
+  // var whatIsHeading = document.getElementById("headingProjects");
+  // var isActive = false;
+  const navLinks = document.querySelectorAll(".nav-link");
 
-var colY = document.getElementById("scrollToTopButton");
+    function setActiveLink(clickedLink) {
+        navLinks.forEach(link => {
+            link.classList.toggle("active-a", link === clickedLink);
+        });
+    }
 
-colY.addEventListener("click", function() {
-  window.scrollTo({
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            setActiveLink(link);
+        });
+    });
+
+
+  colY.addEventListener("click", function () {
+    window.scrollTo({
       top: 0,
       behavior: 'smooth' // Add smooth scrolling behavior
+    });
+  });
+
+  myProjectsBtn.addEventListener("click", () => {
+    myProjects.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+
+  aboutLink.addEventListener("click", () => {
+
+    mySection.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+
   });
 });
+
+
+
+
+
+
 
